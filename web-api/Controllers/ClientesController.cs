818aa132d7429cce287684caf7c8599bb7948690
Gerardo Buenrostro González <blogcraft@gmail.com>
2021-07-05 14:48:36 -0400

    1.14.1 Acciones Obtener cliente y Obtener clientes

--- web-api/Controllers/ClientesController.cs
+++ web-api/Controllers/ClientesController.cs
@@ -18,19 +18,26 @@ namespace web_api.Controllers
             _appdbContext = appdbContext;
         }
 
+        // GET: api/Clientes
         [HttpGet]
-        public ActionResult ObtenerClientes([FromQuery] int limite)
+        public async Task<ActionResult> ObtenerClientes([FromQuery] int limite)
         {
-            Cliente[] clientes = {
-                new() { Nombre = "Juan", Apellicos = "Martinez", Direccion = "Calle A Nº 1234", Telefono = "+56 9 2728 1232"},
-                new() { Nombre = "Pepe", Apellicos = "Martinez", Direccion = "Calle B Nº 1243", Telefono = "+56 9 2827 1232"},
-                new() { Nombre = "Ana", Apellicos = "Martinez", Direccion = "Calle C Nº 1324", Telefono = "+56 9 2728 3212"},
-                new() { Nombre = "Francisca", Apellicos = "Martinez", Direccion = "Calle D Nº 1342", Telefono = "+56 9 2712 2832"}
-            };
+            List<Cliente> clientes = await _appdbContext.Cliente.ToListAsync();
 
             if (!clientes.Any())
                 return NotFound();
             return Ok(clientes.Take((int)limite));
+        }
+
+        // GET: api/Clientes/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult> ObtenerCliente(int id)
+        {
+            Cliente cliente = await _appdbContext.Cliente.FindAsync(id);
+
+            if (cliente == null)
+                return NotFound();
+            return Ok(cliente);
         }
 
         [HttpPost]
