7fe78c6657657157a08d0f9ad5a43a705e2b2b11
Gerardo Buenrostro González <blogcraft@gmail.com>
2021-07-06 17:09:01 -0400

    1.17.3 TransaccionController

--- web-api/Controllers/Negocio/TransaccionController.cs
+++ web-api/Controllers/Negocio/TransaccionController.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using web_api.Services;
 
@@ -8,9 +10,38 @@ namespace web_api.Controllers.Negocio
     public class TransaccionController : ControllerBase
     {
         private readonly ITransaccionService _transaccionService;
+
         public TransaccionController(ITransaccionService transaccionService)
         {
             _transaccionService = transaccionService;
+        }
+
+        // POST: /api/negocio/Transaccion/Comprar
+        [HttpPost("Comprar")]
+        public async Task<IActionResult> Comprar([FromBody] TransaccionReq body)
+        {
+            await _transaccionService.ComprarAsync(body.ProductoId, body.CuentaId, body.Cantidad);
+
+            return Ok();
+        }
+
+        // POST: /api/negocio/Transaccion/Vender
+        [HttpPost("Vender")]
+        public async Task<IActionResult> Vender([FromBody] TransaccionReq body)
+        {
+            await _transaccionService.VenderAsync(body.ProductoId, body.CuentaId, body.Cantidad);
+
+            return Ok();
+        }
+
+        public class TransaccionReq
+        {
+            [Range(1, int.MaxValue)]
+            public int ProductoId { get; set; }
+            [Range(1, int.MaxValue)]
+            public int CuentaId { get; set; }
+            [Range(1, int.MaxValue)]
+            public int Cantidad { get; set; }
         }
     }
 }
