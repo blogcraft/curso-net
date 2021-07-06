99d836f19e737f5d21fdfd5b5bbab8985aa0ea1d
Gerardo Buenrostro González <blogcraft@gmail.com>
2021-07-06 17:05:59 -0400

    1.17.2 ReportesController

--- web-api/Controllers/Negocio/ReportesController.cs
+++ web-api/Controllers/Negocio/ReportesController.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using web_api.Data.AppDb.Model;
 using web_api.Services;
 
 namespace web_api.Controllers.Negocio
@@ -15,3 +18,37 @@ namespace web_api.Controllers.Negocio
             _carteraService = carteraService;
             _clienteService = clienteService;
         }
+
+        // GET: /api/negocio/Reportes/Cartera
+        [HttpGet("Cartera")]
+        public async Task<List<ConsCartRes>> ConsultaCartera([FromQuery] ConsultaCarteraParams param)
+        {
+            return await _carteraService.ConsultaCarteraAsync(
+                param.ClienteId, param.Nombre, param.Apellidos, param.CuentaId, param.NumCuenta, param.NomProducto
+            );
+        }
+
+        // GET: /api/negocio/Reportes/Cliente
+        [HttpGet("Cliente")]
+        public async Task<List<Cliente>> ConsultaCliente([FromQuery] ConsultaClienteParams param)
+        {
+            return await _clienteService.ConsultaClienteAsync(param.ClienteId, param.Nombre, param.Apellidos);
+        }
+
+        public class ConsultaCarteraParams
+        {
+            public int? ClienteId { get; set; }
+            public string Nombre { get; set; }
+            public string Apellidos { get; set; }
+            public int? CuentaId { get; set; }
+            public string NumCuenta { get; set; }
+            public string NomProducto { get; set; }
+        }
+        public class ConsultaClienteParams
+        {
+            public int? ClienteId { get; set; }
+            public string Nombre { get; set; }
+            public string Apellidos { get; set; }
+        }
+    }
+}
