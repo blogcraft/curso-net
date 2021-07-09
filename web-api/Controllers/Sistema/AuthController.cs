bde4ade448d987c7f91da7c01531a4c7827b8d73
Gerardo Buenrostro González <blogcraft@gmail.com>
2021-07-09 14:00:02 -0400

    1.18.7 Iniciar Sesión

---
+++ web-api/Controllers/Sistema/AuthController.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using web_api.Services;
+
+namespace web_api.Controllers.Sistema
+{
+    [Route("api/sistema/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("SignIn")]
+        public async Task<IActionResult> SignIn([FromBody] LoginParameters param)
+        {
+            string result = await _authService.SignIn(param);
+            return Ok(result);
+        }
+    }
+}
--- web-api/Startup.cs
+++ web-api/Startup.cs
@@ -34,10 +34,13 @@ namespace web_api
             services.AddScoped<IClienteService, ClienteService>();
             services.AddScoped<ICarteraService, CarteraService>();
             services.AddSingleton<ICalculoService, CalculoService>();
+            services.AddScoped<IAuthService, AuthService>();
             services.AddScoped<IAccountService, AccountService>();
 
             services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddEntityFrameworkStores<AuthDbContext>();
+
+            services.AddAuthentication();
 
             services.Configure<IdentityOptions>(options =>
                 {
