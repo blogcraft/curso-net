7b7763f20fbd81872ff8ec331dea7f68ea747341
Gerardo Buenrostro González <blogcraft@gmail.com>
2021-07-10 01:11:14 -0400

    1.22 Crear Clientes que Consuman API con Swagger

---
+++ cliente-cli/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Net.Http.Headers;
+
+namespace cliente_cli
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+        }
+    }
+}
