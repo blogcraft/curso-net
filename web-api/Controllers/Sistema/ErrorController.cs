a3d6863ed0b3ebda52b9020fcc97b5ac0b9b93b9
Gerardo Buenrostro González <blogcraft@gmail.com>
2021-07-09 20:37:19 -0400

    1.20.2 ErrorController.cs

---
+++ web-api/Controllers/Sistema/ErrorController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace web_api.Controllers.Sistema
+{
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [Route("/error")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult Error()
+        {
+            var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            return Problem(
+                instance: context.Error.Source,
+                title: context.Error.Message);
+        }
+
+        [Route("/error-debug")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public IActionResult ErrorDebug()
+        {
+            var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+
+            return Problem(
+                instance: context.Error.Source,
+                detail: context.Error.StackTrace,
+                title: context.Error.Message);
+        }
+
+    }
+}
