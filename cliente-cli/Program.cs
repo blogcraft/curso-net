efe4227167a10e2b6be20ad852da37a41d604ab0
Gerardo Buenrostro González <blogcraft@gmail.com>
2021-07-10 01:11:29 -0400

    1.22.1 Cliente

--- cliente-cli/Program.cs
+++ cliente-cli/Program.cs
@@ -10,6 +10,23 @@ namespace cliente_cli
     {
         static async Task Main(string[] args)
         {
+            var httpClient = new HttpClient();
+            var client = new swaggerClient("https://localhost:5001", httpClient);
+
+            var param = new LoginParameters()
+            {
+                UserName = "admin",
+                Password = "Ab12345678"
+            };
+            var token = await client.SignInAsync(body: param);
+
+            Console.WriteLine(token);
+
+            httpClient.DefaultRequestHeaders.Authorization =
+                   new AuthenticationHeaderValue("Bearer", token);
+
+            var u = await client.UsuarioAsync();
+            Console.WriteLine(u.UserName);
         }
     }
 }
